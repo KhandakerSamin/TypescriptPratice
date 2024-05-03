9f955ae019e568f72f269e15079a444a3659b61c
Khandaker Samin Yeasar <138380913+KhandakerSamin@users.noreply.github.com>
2024-05-03 23:08:26 +0600

    Constraint using keyOF

---
+++ M_2_AdvanceTS/2.7.ts
@@ -0,0 +1,49 @@
+{
+    //! Topic : Generic constraint with keyof operator 
+
+    type Vehicle ={
+        bike: string;
+        car: string;
+        ship: string;
+    }
+
+    type Owner1 = "bike" | "car" | "ship"; //menually
+
+    type Owner2 = keyof Vehicle 
+
+    // const parson1 : Owner2 = //? then ctrl + space
+
+
+    const user = {
+        name: "Mr. Samin",
+        age: 21,
+        addrss: 'Dhaka'
+    }
+
+    const car = {
+        model: 'supra',
+        year: 2022
+    }
+
+    // console.log(user.);
+
+    const  getPropertyValue = <X,Y extends keyof X>(obj:X , key: Y) =>{
+        return obj[key];
+    } 
+
+    const result1 = getPropertyValue(user,'emni')
+
+
+
+    
+    // user.name
+
+
+
+
+
+
+
+
+    //
+}
