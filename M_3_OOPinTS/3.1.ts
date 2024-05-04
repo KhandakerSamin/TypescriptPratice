1f733bc617780f41ea2490971f552363aa020c48
Khandaker Samin Yeasar <138380913+KhandakerSamin@users.noreply.github.com>
2024-05-04 23:59:44 +0600

    OOP2  start in TS

--- M_3_OOPinTS/3.1.ts
+++ M_3_OOPinTS/3.1.ts
@@ -29,16 +29,16 @@
 
 
     class Animal {
-        public name: string;
-        public species: string;
-        public sound: string;
+        // public name: string;
+        // public species: string;
+        // public sound: string;
 
         // parameter properties 
 
-        constructor( name: string, species: string, sound: string) {
-            this.name = name;
-            this.species = species;
-            this.sound = sound;
+        constructor(public name: string,public species: string, public sound: string) {
+            // this.name = name;
+            // this.species = species;
+            // this.sound = sound;
         }
 
         makeSound() {
