1ead2a0260f64eecb65984670dc2163f267af6b1
Khandaker Samin Yeasar <138380913+KhandakerSamin@users.noreply.github.com>
2024-05-04 20:51:04 +0600

    Conditional Type in TS

---
+++ M_2_AdvanceTS/2.9.ts
@@ -0,0 +1,45 @@
+{
+    //! Conditional Type
+
+    type a1 = null  //or number
+    type b1 = undefined 
+
+    //? Conditional type x  
+    type x1 = a1 extends null ? true : false ;  // true
+    type x2 = a1 extends null ? true : false ;  // false when a1 number
+
+    type y = a1 extends null ? true : b1 extends undefined ? undefined : any ;
+
+    type Sheikh = {
+        bike : string;
+        car: string;
+        ship: string
+    }
+
+    // car ase kina / bike ase kina / ship ase kina / trak ase kina
+    type CheckVehicle1 <T> = T extends keyof Sheikh ? true : false  // using keyof
+    type CheckVehicle2 <T> = T extends "bike" | "car" | "ship" ? true : false  // hardcore
+    type HasBike = CheckVehicle1<"car">   //true
+    type HasShip = CheckVehicle2<"ship">  //true 
+    type HasTrack = CheckVehicle1<"track">  //false
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //
+}
