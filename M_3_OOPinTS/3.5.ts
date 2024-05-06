2ed49fdfb6f88b7344283afee409eed3469dd5f2
Khandaker Samin Yeasar <138380913+KhandakerSamin@users.noreply.github.com>
2024-05-06 15:26:26 +0600

    Access modifires in TS

---
+++ M_3_OOPinTS/3.5.ts
@@ -0,0 +1,52 @@
+{
+    //! Topic : Access modifiers 
+
+    class BankAccount {
+        public readonly id: number;
+        public name: string ;
+        protected balance: number;
+        constructor(id: number, name:string,balance:number){
+            this.id = id;
+            this.name = name;
+            this.balance = balance;
+        }
+        
+        public addDeposit (ammount : number){
+            this.balance = this.balance + ammount ;
+        }
+
+        public getBalance (){
+            return this.balance ;
+        }
+    }
+
+
+    // class StudentAccount extends BankAccount {
+    //     test(){
+    //         this.
+    //     }
+    // }
+
+    //? using of private and protected modifier
+
+
+    const goribManushErAccunt = new BankAccount(111,'Mr. gorib', 20);
+
+    // goribManushErAccunt.id =222;
+    // goribManushErAccunt.balance=0;
+    goribManushErAccunt.addDeposit(100);
+    const myBalance = goribManushErAccunt.getBalance();
+    console.log(myBalance);
+
+
+
+
+    
+
+
+
+
+
+
+    //
+}
