f1247c6ae17d1642072593e1e05e1bb0cde9b99f
Khandaker Samin Yeasar <138380913+KhandakerSamin@users.noreply.github.com>
2024-05-10 22:24:39 +0600

    Practice problem

--- PracticeProblem/PP-2.ts
+++ PracticeProblem/PP-2.ts
@@ -1,1 +1,11 @@
-//? PP-2 : 
+//? PP-2 : Create a TypeScript function sumOfSquares that takes an array of numbers as input and returns the sum of the squares of those numbers.
+
+function sumOfSquares(numbers: number[]): number {
+    return numbers.reduce((acc, num) => acc + num ** 2, 0);
+}
+
+// Sample Input:
+console.log(sumOfSquares([1, 2, 3, 4]));
+
+// Sample Output:
+// 30 (1^2 + 2^2 + 3^2 + 4^2)
---
+++ PracticeProblem/PP-3.ts
@@ -0,0 +1,2 @@
+//? PP-3 : Design a TypeScript function isPalindrome that takes a string as input and returns true if the string is a palindrome (reads the same forwards and backwards), and false otherwise.
+
